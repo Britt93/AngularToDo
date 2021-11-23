a418d1076a4a15bbd098c552c3abccab77ae13c5
Ooms Britt <57892631+Britt93@users.noreply.github.com>
2021-11-23 14:32:47 +0100

    commentaar bij list-detail

--- todo-app/src/app/list-detail/list-detail.component.ts
+++ todo-app/src/app/list-detail/list-detail.component.ts
@@ -3,9 +3,9 @@ import { List } from '../list';
 import { ListService } from '../list.service';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { Item } from '../item';
+//import { Item } from '../item';
 
-import { ItemService } from '../item.service';
+//import { ItemService } from '../item.service';
 
 
 @Component({
@@ -16,17 +16,14 @@ import { ItemService } from '../item.service';
 export class ListDetailComponent implements OnInit {
   list: List = { id: 0, name: "", color: "" };
 
-  items$: Observable<Item[]> = new Observable<Item[]>();
-  constructor(private listService: ListService, private itemService: ItemService, private route: ActivatedRoute) { }
+  //items$: Observable<Item[]> = new Observable<Item[]>();
+  constructor(private listService: ListService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     const listId = this.route.snapshot.paramMap.get('id');
     console.log(listId);
     if (listId != null) {
-      /*let listTemp = this.listService.getListById(+listId) ?? null;
-      if(listTemp != null) {
-        this.list = listTemp;
-      }*/
+
       this.listService.getListById(+listId).subscribe(result => {
         this.list = result;
       }
